c37c16bc8fd6532914ab25f457b3ab5edeffcb19
drifftr <drifftr@gmail.com>
2023-03-08 15:26:16 +0530

    Add cron check, status update and org module check

--- dist/index.js
+++ dist/index.js
@@ -9352,6 +9352,19 @@ try {
     const commitId = core.getInput('commitId');
     const runId = core.getInput('runId');
     const alertProxyUrl = core.getInput('alertProxyURL');
+    const existingConfigSchemaEncoded = core.getInput('existingConfigSchema');
+    const existingConfigSchemaData = JSON.parse(Buffer.from(existingConfigSchemaEncoded, 'base64'));
+    const cronExpression = core.getInput('cronExpression');
+    const componentType = core.getInput('componentType');
+    const configurableUpdateURL = core.getInput('configurableUpdateURL');
+
+    // Check for cron expression
+    if (componentType == "scheduledTask" && !cronExpression) {
+        console.log("Cron expression not found");
+        sendAlert(alertProxyUrl, appId, envId, apiVersionId, commitId, runId, 102);
+        core.setFailed("Cron expression not found");
+        return;
+    }
 
     console.log("Processing existing input : " + JSON.stringify(configName));
     console.log("Processing new input : " + JSON.stringify(configSchemaData));
@@ -9363,25 +9376,44 @@ try {
 
     let orgName;
 
-    Object.keys(configSchemaData['properties']).forEach(element => {
+    Object.keys(existingConfigSchemaData['properties']).forEach(element => {
         orgName = element;
     });
     if (!orgName) {
-        console.log("No organizations found in the generated config-schema. skipping configurable check");
+        console.log("No organizations found in the existing config-schema. skipping configurable check");
         return;
     }
 
-    const moduleList = Object.keys(configSchemaData['properties'][orgName]['properties']);
+    // Org Module check
+    let orgModuleCheck = false;
+    if ((Object.keys(configSchemaData['properties']).includes(orgName))) {
+        oldModuleName = Object.keys(existingConfigSchemaData['properties'][orgName]['properties'])[0];
+        if(Object.keys(configSchemaData['properties'][orgName]['properties']).includes(oldModuleName)) {
+            orgModuleCheck = true;
+        }
+    }
+
+    if(!orgModuleCheck) {
+        console.log("Ballerina module name/org is changed");
+        sendAlert(alertProxyUrl, appId, envId, apiVersionId, commitId, runId, 101);
+        core.setFailed("Ballerina module name/org is changed");
+        return;
+    }
+
+    const moduleList = Object.keys(existingConfigSchemaData['properties'][orgName]['properties']);
     const configurableData = configSchemaData['properties'][orgName]['properties'][moduleList[0]];
 
     const reqArray = flattenSchemaReq(configurableData['properties'], '', [], configurableData['required']);
     const allArray = flattenSchema(configurableData['properties'], '', []);
     const nonReqArray = reduceArray(allArray, reqArray);
     const configMatch = arrayCompare(configName['data'], reqArray);
+    const outputConfigs = constructOutputArray(allArray, reqArray, configName['data']);
+    fs.writeFileSync(`${subPath}/config-schema-values.json`, outputConfigs, 'utf-8');
 
     if (configMatch) {
         let successMsg = "Configurable Check Success";
         console.log(successMsg);
+        updateRudderWithNewConfigurables(configurableUpdateURL, appId, envId, apiVersionId, commitId, runId, "success");
     } else {
         let errMsg = "Error Occurred: Configurable fields doesn't match, Please retry a manual deployment providing all the corresponding configurable values"          
         if(reduceArray(reqArray, configName['data']).length == 0) {
@@ -9397,22 +9429,23 @@ try {
                 }
                 if(!checkResult) {
                     console.log(errMsg);
-                    sendAlert(alertProxyUrl, appId, envId, apiVersionId, commitId, runId);
+                    sendAlert(alertProxyUrl, appId, envId, apiVersionId, commitId, runId, 101);
                     core.setFailed(errMsg);
                     return errMsg;
                 } else {
                     let successMsg = "Configurable Check Success";
                     console.log(successMsg);
+                    updateRudderWithNewConfigurables(configurableUpdateURL, appId, envId, apiVersionId, commitId, runId, "success");
                 }
             } else {
                 console.log(errMsg);
-                sendAlert(alertProxyUrl, appId, envId, apiVersionId, commitId, runId);
+                sendAlert(alertProxyUrl, appId, envId, apiVersionId, commitId, runId, 101);
                 core.setFailed(errMsg);
                 return errMsg;
             }
         } else {
             console.log(errMsg);
-            sendAlert(alertProxyUrl, appId, envId, apiVersionId, commitId, runId);
+            sendAlert(alertProxyUrl, appId, envId, apiVersionId, commitId, runId, 101);
             core.setFailed(errMsg);
             return errMsg;
         }
@@ -9531,7 +9564,32 @@ function flattenSchema(iterObj, inKey, dots) {
     return dots;
 }
 
-function sendAlert(alertProxyUrl, appId, envId, apiVersionId, commitId, runId) {
+function constructOutputArray(allArray, reqArray, refArray) {
+    const outputArray = [];
+    for (a of allArray) {
+        const outputItem = {
+            "config_key_name": a['key'],
+            "value_type": a['type'],
+            "value_or_source": getValueFromFlattenArray(a, refArray),
+            "is_required": objExistsInArray(a, reqArray),
+        }
+        outputArray.push(outputItem);
+    }
+    return outputArray;
+}
+
+function getValueFromFlattenArray(compObject, inputArray) {
+    let value;
+    for (a of inputArray) { 
+        if (a['key'] == compObject['key'] && a['type'] == compObject['type']) {
+            value = a['valueRef'];
+            break;
+        }
+    }
+    return value;
+}
+
+function sendAlert(alertProxyUrl, appId, envId, apiVersionId, commitId, runId, failureReason) {
     if(alertProxyUrl != "") {
         const payload = {
             "app_id": appId,
@@ -9539,13 +9597,36 @@ function sendAlert(alertProxyUrl, appId, envId, apiVersionId, commitId, runId) {
             "api_version_id": apiVersionId,
             "commitId": commitId,
             "runId": parseInt(runId),
-            "failureReason": 1
+            "failureReason": failureReason
         };
 
         axios.post(alertProxyUrl, payload).then(function (response) {
             core.setOutput("choreo-configurable-check-alert-status", "sent");
             console.log("choreo-configurable-check-alert-status", "sent");
             console.log("Alerting Status : " + response.status);
+          })
+          .catch(function (error) {
+            core.setOutput("choreo-status", "failed");
+            console.log(error);
+          });
+    }   
+}
+
+function updateRudderWithNewConfigurables(configurableUpdateURL, appId, envId, apiVersionId, commitId, runId, status) {
+    if(configurableUpdateURL != "") {
+        const payload = {
+            "app_id": appId,
+            "environment_id": envId,
+            "api_version_id": apiVersionId,
+            "commitId": commitId,
+            "runId": parseInt(runId),
+            "status": status
+        };
+
+        axios.post(configurableUpdateURL, payload).then(function (response) {
+            core.setOutput("choreo-configurable-update-status", "updated");
+            console.log("choreo-configurable-update-status", "updated");
+            console.log("Config Update Status : " + response.status);
           })
           .catch(function (error) {
             core.setOutput("choreo-status", "failed");
